d3480284daa75c50e954da3e382ccd28339c0025
Max Podloobniy <max.podloobniy@gmail.com>
2024-02-24 23:11:40 +0200

    Some updates

--- RLE_decoding.py
+++ RLE_decoding.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 def decode_and_save_rle_vectorized(rle_str, image_id, image_shape=(768, 768)):
-    decoded_image = np.zeros(image_shape, dtype=np.uint8)
+    decoded_mask = np.zeros(image_shape, dtype=np.uint8)
     if rle_str != 'nan':
         pairs = np.array(rle_str.split(), dtype=np.int32)
         start = pairs[::2]
@@ -12,10 +13,15 @@ def decode_and_save_rle_vectorized(rle_str, image_id, image_shape=(768, 768)):
         row = start // image_shape[1]
         col = start % image_shape[1]
         for r, c, l in zip(row, col, length):
-            decoded_image[r, c:c + l] = 255
-        decoded_image = decoded_image.T
-    image_path = f'masks/{image_id}'
-    plt.imsave(image_path, decoded_image)
+            decoded_mask[r, c:c + l] = 255
+        decoded_mask = decoded_mask.T
+        mask_path = f'masks/{image_id}'
+        plt.imsave(mask_path, decoded_mask, cmap='gray')
+
+        image_path = f'images/{image_id}.jpg'
+        target_path = f'images_with_ships/{image_id}.jpg'
+        os.replace(image_path, target_path)
+
 
 
 chunks = pd.read_csv('/Users/maxim/airbus-ship-detection/airbus-ship-detection/train_ship_segmentations_v2.csv', chunksize=4000)
