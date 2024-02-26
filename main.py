b8ceae8f210a3b1b43bcc9f5fa35794562abea40
Max Podloobniy <max.podloobniy@gmail.com>
2024-02-26 22:56:59 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,64 +1,71 @@
-import pandas as pd
-import numpy as np
+import os
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras.backend as K
+from tensorflow import image as tf_image
+from tensorflow import io as tf_io
+from keras.models import load_model
 from PIL import Image
 
 
-# Зчитуємо дані з файлу CSV
-#df = pd.read_csv('/Users/maxim/airbus-ship-detection/airbus-ship-detection/train_ship_segmentations_v2.csv')
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1, 2, 3])
+    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
+    return K.mean((2. * intersection + smooth) / (union + smooth), axis=0)
 
 
-# Function for decoding Run Length Encoding
-def decode_rle(rle_str, img_shape):
-    # Ініціалізуємо пустий масив для розкодованої маски
-    decoded_mask = np.zeros(img_shape, dtype=np.uint8)
+def dice_p_bce(in_gt, in_pred):
+    return 1e-3*tf.keras.losses.binary_crossentropy(in_gt, in_pred) - dice_coef(in_gt, in_pred)
 
-    if rle_str == 'nan':
-        return decoded_mask
+
+custom_losses = {'dice_p_bce': dice_p_bce,
+                 'dice_coef': dice_coef}
+
+ship_detection_model = load_model('/Users/maxim/PycharmProjects/Ship_Detection_Challenge/ship_detection_model.keras')
+print('Ship detection model loaded')
+unet_low_filters_model = load_model('/Users/maxim/PycharmProjects/Ship_Detection_Challenge/unet_low_filters_model.keras', custom_objects=custom_losses)
+print('Ship segmentation model loaded')
+
+
+def preprocess_image(img_path):
+    input_img = tf_io.read_file(img_path)
+    input_img = tf_io.decode_jpeg(input_img, channels=3)
+    input_img = tf_image.resize(input_img, (768, 768))
+    input_img = tf_image.convert_image_dtype(input_img, "float32")
+    input_img = tf.expand_dims(input_img, axis=0)
+    return input_img
+
+
+def visualize_mask(img_path, segmentation_mask):
+    image = Image.open(img_path)
+    plt.subplot(1, 2, 1)
+    plt.imshow(image)
+    plt.title('Input image')
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(segmentation_mask, cmap='gray')
+    plt.title('Results')
+    plt.axis('off')
+
+    plt.show()
+
+
+def predict_and_visualize(image_path):
+    input_img = preprocess_image(image_path)
+    ship_prediction = ship_detection_model.predict(input_img)
+
+    if ship_prediction > 0.6:
+        segmentation_result = unet_low_filters_model.predict(input_img)
+        segmentation_result = tf.squeeze(segmentation_result, axis=0)
+        visualize_mask(image_path, segmentation_result)
     else:
-        # Розбиваємо рядок RLE
-        pairs = rle_str.split()
-
-        # Ітеруємося по парам (start, length) та встановлюємо відповідні значення
-        for i in range(0, len(pairs), 2):
-            start = int(pairs[i])
-            length = int(pairs[i + 1])
-
-            # Розраховуємо розміщення пікселів у двовимірному масиві
-            row = start // img_shape[1]  # Рядок у двовимірному масиві
-            col = start % img_shape[1]  # Стовпець у двовимірному масиві
-
-            # Встановлюємо значення пікселів у відповідних позиціях
-            decoded_mask[row, col:col + length] = 1  # Встановлюємо значення пікселів на 255 (білий)
-
-        return decoded_mask.T
+        print(f'Ships not found on image {image_path}')
 
 
-def decode_and_save_rle_vectorized(rle_str, image_id, image_shape=(768, 768)):
-    decoded_image = np.zeros(image_shape, dtype=np.uint8)
-    if rle_str != 'nan':
-        pairs = np.array(rle_str.split(), dtype=np.int32)
-        start = pairs[::2]
-        length = pairs[1::2]
-        row = start // image_shape[1]
-        col = start % image_shape[1]
-        for r, c, l in zip(row, col, length):
-            decoded_image[r, c:c + l] = 255
-        decoded_image = decoded_image.T
-    image_path = f'masks/{image_id}'
-    plt.imsave(image_path, decoded_image)
+test_img_dir = '/Users/maxim/airbus-ship-detection/airbus-ship-detection/test_v2'
+test_img_list = sorted(os.listdir(test_img_dir))[:11]
 
-
-chunks = pd.read_csv('/Users/maxim/airbus-ship-detection/airbus-ship-detection/train_ship_segmentations_v2.csv',
-                     chunksize=4000)
-print('File read')
-
-for i, chunk in enumerate(chunks):
-    if i % 2 == 0:
-        print(f'Processing {i} chunk')
-    grouped_df = chunk.groupby('ImageId')['EncodedPixels'].apply(lambda x: x.str.cat(sep=' ')).reset_index()
-    grouped_df['EncodedPixels'] = grouped_df.apply(
-        lambda row: decode_and_save_rle_vectorized(row['EncodedPixels'], row['ImageId']), axis=1)
-
-
-
+for image_id in test_img_list:
+    full_path = os.path.join(test_img_dir, image_id)
+    predict_and_visualize(full_path)
