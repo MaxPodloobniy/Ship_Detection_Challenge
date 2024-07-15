4b176d886ef7cab284f0126a8344786f072f42fc
Max Podloobniy <maxim@MAXIMs-MacBook-Pro.local>
2024-07-15 14:31:48 +0300

    Update model evaluation: Compare v1 & v2, add GitHub/local image input

--- model_evaluation.py
+++ model_evaluation.py
@@ -6,7 +6,7 @@ from tensorflow import io as tf_io
 from tensorflow import keras
 from PIL import Image
 
-
+segmentation_model_v1 = keras.models.load_model('ship_segmentation/ship_segmentation_model.keras', compile=False)
 segmentation_model_v2 = keras.models.load_model('ship_segmentation/ship_segmentation_model_v2.keras', compile=False)
 print('Ship segmentation model loaded')
 
@@ -20,16 +20,23 @@ def preprocess_image(img_path):
     return input_img
 
 
-def visualize_mask(img_path, segmentation_mask):
+def visualize_masks(img_path, seg_mask_v1, seg_mask_v2):
     image = Image.open(img_path)
-    plt.subplot(1, 2, 1)
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 3, 1)
     plt.imshow(image)
-    plt.title('Input image')
+    plt.title('Input Image')
     plt.axis('off')
 
-    plt.subplot(1, 2, 2)
-    plt.imshow(segmentation_mask, cmap='gray')
-    plt.title('Results')
+    plt.subplot(1, 3, 2)
+    plt.imshow(seg_mask_v1, cmap='gray')
+    plt.title('Model v1 Result')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(seg_mask_v2, cmap='gray')
+    plt.title('Model v2 Result')
     plt.axis('off')
 
     plt.show()
@@ -38,13 +45,20 @@ def visualize_mask(img_path, segmentation_mask):
 def predict_and_visualize(image_path):
     input_img = preprocess_image(image_path)
 
-    segmentation_result = segmentation_model_v2.predict(input_img)
-    segmentation_result = tf.squeeze(segmentation_result, axis=0)
-    visualize_mask(image_path, segmentation_result)
+    seg_result_v1 = segmentation_model_v1.predict(input_img)
+    seg_result_v1 = tf.squeeze(seg_result_v1, axis=0)
+
+    seg_result_v2 = segmentation_model_v2.predict(input_img)
+    seg_result_v2 = tf.squeeze(seg_result_v2, axis=0)
+
+    visualize_masks(image_path, seg_result_v1, seg_result_v2)
 
 
-test_img_dir = '/Users/maxim/airbus-ship-detection/test_v2'
-test_img_list = sorted(os.listdir(test_img_dir))[140:160]
+test_img_dir = 'test_imgs'
+test_img_list = sorted(os.listdir(test_img_dir))
+
+# test_img_dir = '/Users/maxim/airbus-ship-detection/test_v2'
+# test_img_list = sorted(os.listdir(test_img_dir))[160:180]
 
 for image_id in test_img_list:
     full_path = os.path.join(test_img_dir, image_id)
